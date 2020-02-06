ba5e258fa60f08b3099ca50aaa1a41681dc7d2d4
Jens Laufer <jenslaufer@gmail.com>
2020-02-06 13:46:31 +0100

    added import script

--- src/import.py
+++ src/import.py
@@ -1,3 +1,12 @@
 
-typ
-time_url_template = f"https://docs.google.com/spreadsheets/d/1UF2pSkFTURko2OvfHWWlFpDFAr1UxCBA4JLwlSP6KFo/export?format=csv#gid={id}"
+import requests
+
+url = f"https://docs.google.com/spreadsheets/d/1UF2pSkFTURko2OvfHWWlFpDFAr1UxCBA4JLwlSP6KFo/export?format=csv"
+
+sheets = ['Confirmed', 'Deaths', 'Recovered']
+
+
+for sheet in sheets:
+    r = requests.get(url)
+    with open(f'data/{sheet}.csv', 'wb') as f:
+        f.write(r.content)
