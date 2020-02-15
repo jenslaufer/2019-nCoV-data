3a88fbbcdc86f9b816907d81d84926948d2a4e83
Jens Laufer <jenslaufer@gmail.com>
2020-02-15 20:14:24 +0100

    changed

--- src/import.py
+++ src/import.py
@@ -2,14 +2,12 @@
 import requests
 import pandas as pd
 
-url = "https://docs.google.com/spreadsheets/d/1UF2pSkFTURko2OvfHWWlFpDFAr1UxCBA4JLwlSP6KFo/export?format=xlsx"
+url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-{0}.csv"
 
-r = requests.get(url)
-with open('data/data.xlsx', 'wb') as f:
-    f.write(r.content)
 
-sheets = ['Confirmed', 'Death', 'Recovered']
+sheets = ['Confirmed', 'Deaths', 'Recovered']
 
 for sheet in sheets:
-    pd.read_excel("data/data.xlsx",
-                  sheet_name=sheet).to_csv(f"data/{sheet}.csv", index=None)
+    r = requests.get(url.format(sheet))
+    with open('data/{0}.csv'.format(sheet), 'wb') as f:
+        f.write(r.content)
