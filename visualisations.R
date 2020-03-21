4e753774c05d25b5c8f3fd93f8630d584ced158c
jenslaufer <jenslaufer@gmail.com>
2020-03-21 10:20:53 +0100

    moved on

--- visualisations.R
+++ visualisations.R
@@ -1,6 +1,5 @@
 
 
-
 cases.timelime <- function(data, .trans = "log10") {
   .palette = "Tableau 20"
   if (data %>% pull(name) %>% unique() %>% length() < 10) {
@@ -21,9 +20,9 @@ cases.timelime <- function(data, .trans = "log10") {
     scale_x_continuous(breaks = seq(min(data$day), max(data$day), 1)) +
     theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
     labs(title = "Confirmed COVID-19 Cases",
-         subtitle = "Cases on logarithmic scale on a certain day of outbreak. Days are centered on the first past after 100 cases.",
+         subtitle = "Cases on a certain day of outbreak. Days are centered on the first past after 100 cases.",
          caption = "Source: Jens Laufer (https://jenslaufer.com) Data: BNO News") +
-    
+  
     bbc_style()
   
   plot
@@ -32,5 +31,5 @@ cases.timelime <- function(data, .trans = "log10") {
 
 german <- function(plot) {
   plot +
-    labs(title = "Bestätigte COVID-19 Fälle", subtitle = "Fallzahlen auf logarithmischer Skala an einem bestimmten Tag nach Erreichen von 100 Fällen")
+    labs(title = "Bestätigte COVID-19 Fälle", subtitle = "Fallzahlen an einem bestimmten Tag nach Erreichen von 100 Fällen")
 }
