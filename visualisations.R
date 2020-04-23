087db5711c4a54aaf4f2e916e252b6dff65d0396
jenslaufer <jenslaufer@gmail.com>
2020-04-23 15:59:46 +0200

    added show noise to plot

--- visualisations.R
+++ visualisations.R
@@ -70,22 +70,31 @@ plot.model.data <-
            .fit.feature = "fit",
            .feature1,
            .feature2,
-           is.date = T) {
+           is.date = T,
+           .show.noise = T) {
     plot <- data %>%
-      ggplot(aes(
+      ggplot() +
+      geom_line(aes(
         x = !!sym(.feature1),
-        y = !!sym(.feature2),
+        y = !!sym(.fit.feature),
         color =
           country_region
-      )) +
-      geom_line(aes(y = !!sym(.fit.feature)), size = 2) +
-      geom_line() +
+      ), size = 2) +
       geom_hline(yintercept = 0) +
       scale_color_tableau() +
       facet_wrap(~ country_region, scales = "free") +
       theme(axis.text.x = element_text(angle = 45, hjust = 1))
     if (is.date) {
       plot <- plot + scale_x_date(date_breaks = "2 day")
+    }
+    if (.show.noise) {
+      plot <- plot +
+        geom_line(aes(
+          x = !!sym(.feature1),
+          y = !!sym(.feature2),
+          color =
+            country_region
+        ))
     }
     plot
   }
